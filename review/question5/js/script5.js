352ef936e933a247d2cd4dee429b066ea7cad90a
Shrook12 <shrook1@gmail.com>
2026-01-17 15:35:38 -0500

    starting question5

---
+++ review/question5/js/script5.js
@@ -0,0 +1,60 @@
+
+function setup() {
+    console.log("go")
+    createCanvas(500, 500);
+
+}
+
+function draw() {
+
+
+}
+
+function drawRectangle() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*function draw() {
+    background(0);
+
+
+
+    let startColor = color(173, 216, 230);
+    let endColor = color(0, 0, 139)
+
+
+    let x = 0;
+    let xSize = 50;
+    let rectheight = height / 3;
+    let y = 0;
+
+    for (let x = 0; x < width; x += rectheight) {
+        let percent = x / width;
+        let c = lerpColor(startColor, endColor, percent);
+        fill(c);
+        noStroke();
+        rect(x, y, rectheight, width);
+    }
+
+    if (mouseX > x && mouseX < x + rectheight && mouseY > y && mouseY < y + width) {
+        fill(0);
+    }
+
+
+}*/
+
+
+
+
+
