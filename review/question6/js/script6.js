c4cb5e24895761f48a4652f0fc22a523fb04bc08
Shrook12 <shrook1@gmail.com>
2026-01-18 14:15:47 -0500

    I did the for loops for the numbers forgot to display test in the middle

--- review/question6/js/script6.js
+++ review/question6/js/script6.js
@@ -9,16 +9,29 @@ let test = {
 
 function setup() {
 
-    createCanvas(500, 500);
+    createCanvas(500, 700);
 
 }
 
 function draw() {
     background("black");
-
+    displaynumber();
 }
 
+function displaynumber() {
+    let position = 40;
+    for (let i = 0; i < 10; i++) {
+        fill(test.fill);
+        textSize(28);
+        text(i, 20 + i * position, 40);
+    }
 
+    for (let j = 0; j < 15; j++) {
+        fill(test.fill);
+        textSize(28);
+        text(j, 40, 70 + j * position);
+    }
+}
 
 
 
