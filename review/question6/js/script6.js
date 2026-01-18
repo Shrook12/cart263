913770a877dd3065df932a6f527e12fe3592baec
Shrook12 <shrook1@gmail.com>
2026-01-18 14:19:22 -0500

    finished question 6

--- review/question6/js/script6.js
+++ review/question6/js/script6.js
@@ -16,19 +16,24 @@ function setup() {
 function draw() {
     background("black");
     displaynumber();
+    displayTest();
 }
-
+function displayTest() {
+    fill(test.fill);
+    textSize(test.textSize);
+    text(test.string, width / 2, height / 2);
+}
 function displaynumber() {
     let position = 40;
     for (let i = 0; i < 10; i++) {
         fill(test.fill);
-        textSize(28);
+        textSize(test.textSize);
         text(i, 20 + i * position, 40);
     }
 
     for (let j = 0; j < 15; j++) {
         fill(test.fill);
-        textSize(28);
+        textSize(test.textSize);
         text(j, 40, 70 + j * position);
     }
 }
