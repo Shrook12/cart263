5ae5ea61a61215f36050ea65e7219f1832c526ec
Shrook12 <shrook1@gmail.com>
2026-01-18 13:49:22 -0500

    starting question6

---
+++ review/question6/js/script6.js
@@ -0,0 +1,42 @@
+
+let test = {
+    string: "test",
+    fill: "white",
+    textSize: 28
+}
+
+
+
+function setup() {
+
+    createCanvas(500, 500);
+
+}
+
+function draw() {
+    background("black");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
