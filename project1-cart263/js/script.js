8742301ffcca38636135f752f446877ec4131d63
Shrook12 <shrook1@gmail.com>
2026-02-24 16:46:03 -0500

    I did some of the structure for html

--- project1-cart263/js/script.js
+++ project1-cart263/js/script.js
@@ -4,4 +4,21 @@ window.onload = function () {
     let canvas = document.getElementById("drawingCanvas");
     let context = canvas.getContext("2d");
 
+    function drawPencil() {
+
+    }
+    function drawInk() {
+
+    }
+    function drawRectangle() {
+
+    }
+    function drawCircles() {
+
+    }
+
+    function textures() {
+
+    }
+
 }
