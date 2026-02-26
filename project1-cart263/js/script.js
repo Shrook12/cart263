375f3a6a27088957e0304d9a637590da0b2185da
Shrook12 <shrook1@gmail.com>
2026-02-25 21:55:46 -0500

    did a pencil now I am trying to do an eraser, for now I am just exploring

--- project1-cart263/js/script.js
+++ project1-cart263/js/script.js
@@ -3,22 +3,57 @@ window.onload = function () {
 
     let canvas = document.getElementById("drawingCanvas");
     let context = canvas.getContext("2d");
+    let buttonPencil = document.getElementById("drawPencil");
+    let buttonErase = document.getElementById("eraser");
+    let drawing = false;
+    let eraser = false;
 
-    function drawPencil() {
+    //mouse events
+    canvas.addEventListener("mousedown", startDrawing);
+    canvas.addEventListener("mousemove", draw);
+    canvas.addEventListener("mouseup", stopDrawing);
 
+    function draw(e) {
+        if (!drawing) return;
+
+        let rect = canvas.getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top;
+
+        context.lineWidth = 4;
+        context.lineCap = "round";
+        context.strokeStyle = "purple";
+
+        context.lineTo(x, y);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(x, y);
     }
-    function drawInk() {
-
+    function startDrawing(e) {
+        drawing = true;
+        draw();
     }
-    function drawRectangle() {
-
-    }
-    function drawCircles() {
-
+    function stopDrawing(e) {
+        drawing = false;
+        context.beginPath();
     }
 
-    function textures() {
 
-    }
+    /*  function drawPencil() {
+ 
+     }
+     function drawInk() {
+ 
+     }
+     function drawRectangle() {
+ 
+     }
+     function drawCircles() {
+ 
+     }
+ 
+     function textures() {
+ 
+     } */
 
 }
