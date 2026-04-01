de118be339e7c4dab4c7641cf5890f20e71cd129
Shrook12 <shrook1@gmail.com>
2026-04-01 11:15:29 -0400

    starting task7

---
+++ three-js-starter-models/main.js
@@ -0,0 +1,177 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+
+
+//gltf loader
+const gltfLoader = new GLTFLoader();
+
+// canvas
+const canvas = document.querySelector("canvas#three-ex");
+const scene = new THREE.Scene();
+
+
+
+//lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 50)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight);
+
+//sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
+
+// controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+
+//renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height);
+
+
+//make a plane
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(5, 5),
+    new THREE.MeshStandardMaterial({ color: "#657997" }),
+)
+plane.rotation.x = - Math.PI * 0.5
+plane.position.y = - 0.65
+
+scene.add(plane)
+
+const spotLight = new THREE.SpotLight(0xBB76E3, 20, 10, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0, 2, 3)
+scene.add(spotLight)
+
+//add gltf model
+
+let gltfModel = null;
+let gltfDuck = null;
+try {
+    gltfModel = await gltfLoader.loadAsync('model/Fox/glTF/Fox.gltf');
+    gltfDuck = await gltfLoader.loadAsync("model/Duck/glTF/Duck.gltf");
+    console.log(gltfModel)
+    let objs = []
+    objs.push(gltfModel)
+    objs.push(gltfDuck)
+    addAndRun(objs)
+}
+catch (error) {
+    console.log(error.message)
+}
+try {
+    gltfModel = await gltfLoader.loadAsync("model/Fox/glTF/Fox.gltf");
+    addAndRun(gltfModel)
+
+} catch (error) {
+    console.log(error.message);
+}
+
+function addAndRun(loadedObjsArray) {
+    let foxModel = loadedObjsArray[0].scene.children[0]
+    let duckModel = loadedObjsArray[1].scene.children[0]
+    //the loaded gltf 
+    console.log(loadedObjsArray[0])
+
+    foxModel.scale.set(.015, .015, .015)
+
+    console.log(duckModel.scale)
+    //set scale
+    duckModel.scale.x -= .005
+    duckModel.scale.y -= .005
+    duckModel.scale.z -= .005
+
+    //set pos
+    duckModel.position.x = 1
+    spotLight.target = duckModel
+    //move fox
+    foxModel.position.z = -5
+
+
+    const mixer = new THREE.AnimationMixer(foxModel)
+
+    // Access the second animation clip in the array
+    const clip = loadedObjsArray[0].animations[1];
+
+    //returns a reference to an AnimationAction
+    //https://threejs.org/docs/index.html?q=Anima#AnimationAction
+    const anim_action = mixer.clipAction(clip);
+
+    // Start playing the animation
+    anim_action.play()
+
+
+    scene.add(foxModel)
+    scene.add(duckModel)
+
+    let elapsedTime = 0; //
+    window.requestAnimationFrame(animate);
+
+    function animate(timer) {
+        //calculate the difference since last frame
+        let deltaTime = (timer - elapsedTime) / 1000; //put in secs
+        elapsedTime = timer; //update  new elapsedTime
+
+        // Update controls
+        controls.update();
+        if (mixer) {
+            mixer.update(deltaTime);
+        }
+
+        // fox model
+        foxModel.position.z += .05
+        // Render
+        renderer.render(scene, camera);
+
+        window.requestAnimationFrame(animate);
+    }
+}
+
+/* function addAndRun(loadedObj) {
+    console.log(loadedObj);
+    let foxModel = loadedObj.scene.children[0]
+    foxModel.scale.set(.015, .015, .015)
+    scene.add(foxModel);
+    window.requestAnimationFrame(animate);
+
+    function animate() {
+        // Update controls
+        controls.update();
+        // Render
+        renderer.render(scene, camera);
+        window.requestAnimationFrame(animate);
+    }
+} */
+
+
+/* window.requestAnimationFrame(animate)
+
+function animate() {
+
+    // Update controls
+    controls.update()
+    // Render
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(animate)
+}
+ */
---
+++ threejs-tutorials/tutorial1/script2.js
@@ -0,0 +1,91 @@
+// library ref: because we are loading a module
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+
+const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper())
+
+//scene color
+scene.background = new THREE.Color("skyblue")
+
+///// it's possible to add a background image
+//scene.background = new Three.TextureLoader().load("")
+
+/////it's also possible to make the background be profound and add images to it
+//scene.background = new THREE.CubeTextureLoader()
+//.setPath("")
+//.load("image1","image2","image3","image4","image5","image6")
+
+/////can also make
+//scene.backgroundBlurriness=0.5
+
+/////CAN ALSO HAVE MULTIPLE SCENES: SCENEA, SCENEB, SCENEC
+
+//camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight)
+scene.add(camera)
+//move camera
+//camera.position.z = 1.5
+camera.position.set(0, 2, 3)
+
+
+//access the canvas
+const canvas = document.querySelector("canvas#three-ex")
+//renderer to visualize in 3d
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+//a resize event every time the window is resized
+window.addEventListener("resize", function () {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    //if ondemand rendering this will help to have it also here
+    // renderer.render(scene, camera)
+})
+
+
+//give the ability to see shape from differemt view by moving the mouse
+const controls = new OrbitControls(camera, canvas)
+
+const light = new THREE.PointLight(0xffffff, 400)
+light.position.set(10, 10, 10)
+scene.add(light)
+//this is on demand rendering instead of rendering every frame
+/* controls.addEventListener("change", function () {
+    renderer.render(scene, camera)
+}) */
+
+const object1 = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshPhongMaterial({ color: 0xff0000 }))
+object1.position.set(4, 0, 0)
+scene.add(object1)
+object1.add(new THREE.AxesHelper(5))
+
+const object2 = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshPhongMaterial({ color: 0x00ff00 }))
+object2.position.set(4, 0, 0)
+object1.add(object2)
+object2.add(new THREE.AxesHelper(5))
+
+const object3 = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshPhongMaterial({ color: 0x0000ff }))
+object2.position.set(4, 0, 0)
+scene.add(object3)
+object3.add(new THREE.AxesHelper(5))
+
+//const clock = new THREE.Clock()
+//let delta
+function animate() {
+    requestAnimationFrame(animate)
+
+
+    // cube.rotation.x += 2 * delta
+    //cube.rotation.y += 2 * delta
+
+    controls.update()
+    camera.lookAt(0, 0.5, 0)
+
+    renderer.render(scene, camera)
+}
+
+animate() 
